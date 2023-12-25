b5bb055d3dc58db9879605d106ccb499f2ddaccf
lam <i.mailed.lam@gmail.com>
2023-12-25 21:03:48 +0200

    DEbugging

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const person = {
+  name: "Alice",
+  lastName: "Parker",
+  student: True
+};
+
+console.log(person.student);
+
+console.log(person.lastName);
+
+console.log(person.name);
+
+person.totalSteps = 200;
+
+person.walk = function(steps) {
+  this.totalSteps += steps;
+};
+
+person.walk(250);
+
+console.log(person.totalSteps);
+
